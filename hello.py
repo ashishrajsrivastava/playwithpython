14bba0a45606961d69a4412957e97ca241df4f4e
Ashish Raj <Ashish.Raj@emc.com>
2019-12-12 14:54:22 +0530

    first python

---
+++ hello.py
@@ -0,0 +1,36 @@
+import tkinter as tk 
+from tkinter import *
+
+# creating main tkinter window/toplevel 
+master = tk.Tk()
+master.geometry("180x180+700+300")
+
+# this wil create a label widget 
+l1 = Label(master, text = "Delhi") 
+l2 = Label(master, text = "Mumbai")
+l3 = Label(master, text = "Kolkata")
+l4 = Label(master, text = "Chennai")
+l5 = Label(master, text = "Bengaluru")
+
+
+# rows and columns as specified 
+l1.grid(row = 0, column = 0, sticky = W, pady = 2) 
+l2.grid(row = 1, column = 0, sticky = W, pady = 2)
+l3.grid(row = 2, column = 0, sticky = W, pady = 2)
+l4.grid(row = 3, column = 0, sticky = W, pady = 2)
+l5.grid(row = 4, column = 0, sticky = W, pady = 2)
+
+#Button for click
+b1 = Button(master, text = "Polution Level", background="#00a9e0")
+b2 = Button(master, text = "Polution Level", background="#00a9e0")
+b3 = Button(master, text = "Polution Level", background="#00a9e0")
+b4 = Button(master, text = "Polution Level", background="#00a9e0")
+b5 = Button(master, text = "Polution Level", background="#00a9e0")
+#arranging buttons
+b1.grid(row = 0, column = 1, sticky = W, pady = 2)
+b2.grid(row = 1, column = 1, sticky = W, pady = 2)
+b3.grid(row = 2, column = 1, sticky = W, pady = 2)
+b4.grid(row = 3, column = 1, sticky = W, pady = 2)
+b5.grid(row = 4, column = 1, sticky = W, pady = 2)
+
+mainloop()

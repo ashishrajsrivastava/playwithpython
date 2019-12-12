315ffe79d6bd01b2d57254e2e3898b8b5d779feb
Ashish Raj <Ashish.Raj@emc.com>
2019-12-12 16:09:05 +0530

    new window on clcik

--- hello.py
+++ hello.py
@@ -5,6 +5,14 @@ from tkinter import *
 master = tk.Tk()
 master.geometry("180x180+700+300")
 
+#Func calling next window with details
+def GetDetails(city):
+    newwin = tk.Toplevel(master)
+    newwin.geometry("250x250+700+500")
+    l1 = Label(newwin, text=city)
+    l1.pack()
+
+
 # this wil create a label widget 
 l1 = Label(master, text = "Delhi") 
 l2 = Label(master, text = "Mumbai")
@@ -21,7 +29,7 @@ l4.grid(row = 3, column = 0, sticky = W, pady = 2)
 l5.grid(row = 4, column = 0, sticky = W, pady = 2)
 
 #Button for click
-b1 = Button(master, text = "Polution Level", background="#00a9e0")
+b1 = Button(master, text = "Polution Level", background="#00a9e0" , command = lambda: GetDetails("Delhi"))
 b2 = Button(master, text = "Polution Level", background="#00a9e0")
 b3 = Button(master, text = "Polution Level", background="#00a9e0")
 b4 = Button(master, text = "Polution Level", background="#00a9e0")
